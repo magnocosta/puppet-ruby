ee38a4edcd6e053e24c3e207b3ff89c380729311
Magno Costa <magnocosta.br@gmail.com>
2015-01-19 21:33:29 -0200

    Add check certificate rvm install

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class ruby::install {
     $RUBY_VERSION  = "ruby-2.0.0-p247, 2.1.2"
 
     exec { "rvm-install":
-        command  => "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 && curl -sSL https://get.rvm.io | sudo bash -s stable",
+        command  => "curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import - && curl -sSL https://get.rvm.io | sudo bash -s stable",
         path     => ["/usr/bin", "/bin"],
         unless   => "rvm --version | grep -E 'rvm' -c",
         # Depends basic package
